62ecaacb96e7913c1d96f6376f1278bd9eb2cca7
EternalProg <eternal.proglife@gmail.com>
2024-03-07 21:32:30 +0200

    Some changes

--- Linked List/Doubly Linked List/doubly-linked-list.cpp
+++ Linked List/Doubly Linked List/doubly-linked-list.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <unordered_map>
+#include <cassert>
+#include <stdexcept>
+
+using namespace std;
 
 template <typename T>
 struct Node
@@ -9,6 +14,13 @@ struct Node
 
     Node() {}
     Node(const T &value) : value(value) {}
+
+    friend void swap(Node<T>& lhs, Node<T>& rhs) {
+        using std::swap;
+        swap(lhs.next, rhs.next);
+        swap(lhs.prev, rhs.prev);
+        swap(lhs.value, rhs.value);
+    }
 };
 
 template <typename T>
@@ -17,16 +29,16 @@ class DoublyLinkedList
 private:
     Node<T> *head = nullptr;
     Node<T> *tail = nullptr;
-    size_t size = 0;
+    size_t _size = 0;
 
 public:
-    DoublyLinkedList();
+    DoublyLinkedList() {};
     explicit DoublyLinkedList(size_t sz, const T &value);
     DoublyLinkedList(const DoublyLinkedList &other);
     DoublyLinkedList(DoublyLinkedList &&other) noexcept;
     DoublyLinkedList(const std::initializer_list<T> &init);
 
-    ~DoublyLinkedList(){};
+    ~DoublyLinkedList();
 
     void push_back(const T &value);
     void push_front(const T &value);
@@ -35,9 +47,8 @@ public:
     void pop_front();
 
     // This operation reverses the nodes in the doubly linked list so that the first node becomes the last node while the last node becomes the first node.
-    void reverse(); //
+    void reverse();
 
-    // return first/last element
     T &front();
     T &back();
 
@@ -46,16 +57,18 @@ public:
 
     void clear(); //
 
-    void swap(); //
-
+    template <typename U>
+    friend void swap(DoublyLinkedList<U>& lhs, DoublyLinkedList<U>& rhs);
     // removes duplicates
-    void unique(); //
+    void unique();
 
-    void sort(); //
+    void sort();
+
+    void printList();
 
     friend auto operator<=>(const DoublyLinkedList &lhs, const DoublyLinkedList &rhs) noexcept
     {
-        return lhs.size <=> rhs.size;
+        return lhs._size <=> rhs._size;
     }
 };
 
@@ -73,7 +86,7 @@ DoublyLinkedList<T>::DoublyLinkedList(size_t sz, const T &value)
         current = current->next;
     }
     tail = current;
-    size = sz;
+    _size = sz;
 }
 
 template <typename T>
@@ -88,7 +101,7 @@ DoublyLinkedList<T>::~DoublyLinkedList()
 }
 
 template <typename T>
-DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList &other) : size(other.size)
+DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList &other) : _size(other._size)
 {
     head = new Node(other.head->value);
 
@@ -105,11 +118,11 @@ DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList &other) : size(othe
 }
 
 template <typename T>
-DoublyLinkedList<T>::DoublyLinkedList(DoublyLinkedList &&other) noexcept : head(other.head), tail(other.tail), size(other.size)
+DoublyLinkedList<T>::DoublyLinkedList(DoublyLinkedList &&other) noexcept : head(other.head), tail(other.tail), _size(other._size)
 {
     other.head = nullptr;
     other.tail = nullptr;
-    other.size = 0;
+    other._size = 0;
 }
 
 template <typename T>
@@ -124,13 +137,13 @@ DoublyLinkedList<T>::DoublyLinkedList(const std::initializer_list<T> &init)
 template <typename T>
 size_t DoublyLinkedList<T>::size() const noexcept
 {
-    return size;
+    return _size;
 }
 
 template <typename T>
 bool DoublyLinkedList<T>::empty() const noexcept
 {
-    return size == 0;
+    return _size == 0;
 }
 
 template <typename T>
@@ -147,7 +160,7 @@ void DoublyLinkedList<T>::push_back(const T &value)
         tail->next->prev = tail;
         tail = tail->next;
     }
-    ++size;
+    ++_size;
 }
 
 template <typename T>
@@ -164,65 +177,224 @@ void DoublyLinkedList<T>::push_front(const T &value)
         head->prev->next = head;
         head = head->prev;
     }
-    ++size;
+    ++_size;
 }
 
 template <typename T>
 void DoublyLinkedList<T>::pop_back()
 {
-    if (size == 0)
+    if (_size == 0)
     {
         return;
     }
-    if (size == 1)
+    if (_size == 1)
     {
         delete head;
         head = nullptr;
         tail = nullptr;
-        size = 0;
+        _size = 0;
         return;
     }
     Node<T> *current = tail;
     tail = tail->prev;
     delete current;
     tail->next = nullptr;
-    --size;
+    --_size;
 }
 
 template <typename T>
 void DoublyLinkedList<T>::pop_front()
 {
-    if (size == 0)
+    if (_size == 0)
     {
         return;
     }
-    if (size == 1)
+    if (_size == 1)
     {
         delete head;
         head = nullptr;
         tail = nullptr;
-        size = 0;
+        _size = 0;
         return;
     }
     Node<T> *current = head;
     head = head->next;
     delete current;
     head->prev = nullptr;
-    --size;
+    --_size;
 }
 
 template <typename T>
-T &DoublyLinkedList<T>::front()
+T& DoublyLinkedList<T>::front()
 {
-    if (size == 0)
+    if (_size == 0)
         throw std::out_of_range("List is empty");
     return head->value;
 }
 
 template <typename T>
-T &DoublyLinkedList<T>::front()
+T& DoublyLinkedList<T>::back()
 {
-    if (size == 0)
+    if (_size == 0)
         throw std::out_of_range("List is empty");
-    return head->value;
+    return tail->value;
+}
+
+template <typename T>
+void DoublyLinkedList<T>::unique()
+{
+    std::unordered_map<T, bool> uniqueMap;
+
+    Node<T> *current = head;
+    while (current)
+    {
+        if (uniqueMap[current->value])
+        {
+            Node<T> *temp = current;
+            if (current->prev) current->prev->next = current->next;
+            if (current->next) current->next->prev = current->prev;
+            if (current == tail)
+            {
+                tail = current->prev;
+            }
+            current = current->next;
+            delete temp;
+            --_size;
+        }
+        else
+        {
+            uniqueMap[current->value] = true;
+            current = current->next;
+        }
+    }
+}
+
+template <typename T>
+void DoublyLinkedList<T>::reverse()
+{
+    Node<T> *current = head;
+    Node<T> *temp = nullptr;
+
+    while (current)
+    {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+
+    if (temp)
+    {
+        head = temp->prev;
+    }
+}
+
+template <typename T>
+void swap(DoublyLinkedList<T>& lhs, DoublyLinkedList<T>& rhs) {
+    using std::swap;
+    swap(lhs.head, rhs.head);
+    swap(lhs.tail, rhs.tail);
+    swap(lhs._size, rhs._size);
+}
+/*
+template <typename T>
+void DoublyLinkedList<T>::sort() {
+    if (_size <= 1) {
+        return;
+    }
+    Node<T>* current = head;
+    Node<T>* temp = nullptr;
+
+    while (current) {
+        temp = current->next;
+        while (temp) {
+            if (current->value > temp->value) {
+                // Використовуємо функцію swap для обміну вузлів
+                swap(*current, *temp);
+            }
+            temp = temp->next;
+        }
+        current = current->next;
+    }
+}
+*/
+
+template <typename T>
+void DoublyLinkedList<T>::printList()
+{
+    Node<T>* current = head;
+
+    while(current) {
+        std::cout << current->value << ' ';
+        current = current->next;
+    }
+
+    std::cout << '\n';
+}
+
+void runTests() {
+    // Test case 1: Create an empty list
+    DoublyLinkedList<int> emptyList;
+    assert(emptyList.empty());
+
+    // Test case 2: Create a list with initial values
+    DoublyLinkedList<int> list1 = {1, 2, 3};
+    assert(list1.size() == 3);
+    assert(list1.front() == 1);
+    assert(list1.back() == 3);
+
+    // Test case 3: Push elements to the back of the list
+    list1.push_back(4);
+    assert(list1.size() == 4);
+    assert(list1.back() == 4);
+
+    // Test case 4: Push elements to the front of the list
+    list1.push_front(0);
+    assert(list1.size() == 5);
+    assert(list1.front() == 0);
+
+    // Test case 5: Pop elements from the front of the list
+    list1.pop_front();
+    assert(list1.size() == 4);
+    assert(list1.front() == 1);
+
+    // Test case 6: Pop elements from the back of the list
+    list1.pop_back();
+    assert(list1.size() == 3);
+    assert(list1.back() == 3);
+
+    // Test case 7: Remove duplicate elements from the list
+    list1.push_back(2);
+    list1.push_back(3);
+    list1.unique();
+    assert(list1.size() == 3);
+    assert(list1.front() == 1);
+    assert(list1.back() == 3);
+
+    DoublyLinkedList<int> list = {1, 15, 3, 4, 4, 2, 7, 8, 23, 10};
+
+    list.push_back(11);
+    list.push_front(0);
+    list.printList();
+
+    list.pop_front();
+    list.pop_back();
+    list.printList();
+
+    list.unique();
+    list.printList();
+
+    list.reverse();
+    list.printList();
+
+    list.sort();
+    list.printList();
+
+    std::cout << "All tests passed!" << std::endl;
+}
+
+int main() {
+    runTests();
+
+    return 0;
 }
